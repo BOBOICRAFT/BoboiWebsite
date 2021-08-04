cc6095e2334f7f3c6bba30030e8eecb38cf94f35
BOBOICRAFT <75966266+BOBOICRAFT@users.noreply.github.com>
2021-08-04 19:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function jump(){
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    },500);
+    },300);
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
